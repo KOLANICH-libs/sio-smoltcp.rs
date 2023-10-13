615fa0fe59f61d635ea30ee6b720acd9b692e410
KOLANICH <kolan_n@mail.ru>
2023-10-14 01:02:56 +0300

    We had to move to Codeberg

--- python/sio_smoltcp/__init__.py
+++ python/sio_smoltcp/__init__.py
@@ -1,3 +1,7 @@
 import ctypes
 import sys
 from ctypes import *
+
+from warnings import warn
+
+warn("We have moved from M$ GitHub to https://codeberg.org/KOLANICH-libs/sio-smoltcp.rs , read why on https://codeberg.org/KOLANICH/Fuck-GuanTEEnomo .")
